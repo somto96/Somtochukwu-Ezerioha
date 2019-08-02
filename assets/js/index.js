f1bd3d8bfe024943d1c8f1ff6cad3d6e1fc87b6e
Somto96 <motivatedsomt@gmail.com>
2019-08-02 13:42:53 +0100

    Footer and contct me sections

---
+++ assets/js/index.js
@@ -0,0 +1,360 @@
+// Form Handling and Validation
+var submitHandler = function (event) {
+    event.preventDefault();
+    $("form").on('submit', () => { });
+    var values = {}
+    $.each($("form").serializeArray(), function (i, field) {
+        values[field.name] = field.value;
+    })
+    console.log(values);
+    let param = {
+        from: `${values.email}`,
+        subject: `New Contact ${values.fullname}`,
+        message: `
+      <!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+	xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+	<meta charset="utf-8"> <!-- utf-8 works for most cases -->
+	<meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+	<meta name="x-apple-disable-message-reformatting"> <!-- Disable auto-scale in iOS 10 Mail entirely -->
+	<title></title> <!-- The title tag shows in email notifications, like Android 4.4. -->
+
+	<!-- Web Font / @font-face : BEGIN -->
+	<!-- NOTE: If web fonts are not required, lines 10 - 27 can be safely removed. -->
+
+	<!-- Desktop Outlook chokes on web font references and defaults to Times New Roman, so we force a safe fallback font. -->
+	<!--[if mso]>
+        <style>
+            * {
+                font-family: sans-serif !important;
+            }
+        </style>
+    <![endif]-->
+
+	<!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ -->
+	<!--[if !mso]><!-->
+	<!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> -->
+	<!--<![endif]-->
+
+	<!-- Web Font / @font-face : END -->
+
+	<!-- CSS Reset : BEGIN -->
+	<style>
+		/* What it does: Remove spaces around the email design added by some email clients. */
+		/* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
+		html,
+		body {
+			margin: 0 auto !important;
+			padding: 0 !important;
+			height: 100% !important;
+			width: 100% !important;
+		}
+
+		/* What it does: Stops email clients resizing small text. */
+		* {
+			-ms-text-size-adjust: 100%;
+			-webkit-text-size-adjust: 100%;
+		}
+
+		/* What it does: Centers email on Android 4.4 */
+		div[style*="margin: 16px 0"] {
+			margin: 0 !important;
+		}
+
+		/* What it does: Stops Outlook from adding extra spacing to tables. */
+		table,
+		td {
+			mso-table-lspace: 0pt !important;
+			mso-table-rspace: 0pt !important;
+		}
+
+		/* What it does: Fixes webkit padding issue. */
+		table {
+			border-spacing: 0 !important;
+			border-collapse: collapse !important;
+			table-layout: fixed !important;
+			margin: 0 auto !important;
+		}
+
+		/* What it does: Prevents Windows 10 Mail from underlining links despite inline CSS. Styles for underlined links should be inline. */
+		a {
+			text-decoration: none;
+		}
+
+		/* What it does: Uses a better rendering method when resizing images in IE. */
+		img {
+			-ms-interpolation-mode: bicubic;
+		}
+
+		/* What it does: A work-around for email clients meddling in triggered links. */
+		a[x-apple-data-detectors],
+		/* iOS */
+		.unstyle-auto-detected-links a,
+		.aBn {
+			border-bottom: 0 !important;
+			cursor: default !important;
+			color: inherit !important;
+			text-decoration: none !important;
+			font-size: inherit !important;
+			font-family: inherit !important;
+			font-weight: inherit !important;
+			line-height: inherit !important;
+		}
+
+		/* What it does: Prevents Gmail from changing the text color in conversation threads. */
+		.im {
+			color: inherit !important;
+		}
+
+		/* What it does: Prevents Gmail from displaying a download button on large, non-linked images. */
+		.a6S {
+			display: none !important;
+			opacity: 0.01 !important;
+		}
+
+		/* If the above doesn't work, add a .g-img class to any image in question. */
+		img.g-img+div {
+			display: none !important;
+		}
+
+		/* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */
+		/* Create one of these media queries for each additional viewport size you'd like to fix */
+
+		/* iPhone 4, 4S, 5, 5S, 5C, and 5SE */
+		@media only screen and (min-device-width: 320px) and (max-device-width: 374px) {
+			u~div .email-container {
+				min-width: 320px !important;
+			}
+		}
+
+		/* iPhone 6, 6S, 7, 8, and X */
+		@media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
+			u~div .email-container {
+				min-width: 375px !important;
+			}
+		}
+
+		/* iPhone 6+, 7+, and 8+ */
+		@media only screen and (min-device-width: 414px) {
+			u~div .email-container {
+				min-width: 414px !important;
+			}
+		}
+	</style>
+
+	<!-- What it does: Makes background images in 72ppi Outlook render at correct size. -->
+	<!--[if gte mso 9]>
+    <xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+
+	<!-- CSS Reset : END -->
+
+	<!-- Progressive Enhancements : BEGIN -->
+	<style>
+		/* What it does: Hover styles for buttons */
+		.button-td,
+		.button-a {
+			transition: all 100ms ease-in;
+		}
+
+		.button-td-primary:hover,
+		.button-a-primary:hover {
+			background: #555555 !important;
+			border-color: #555555 !important;
+		}
+
+		/* Media Queries */
+		@media screen and (max-width: 600px) {
+
+			.email-container {
+				width: 100% !important;
+				margin: auto !important;
+			}
+
+			/* What it does: Forces table cells into full-width rows. */
+			.stack-column,
+			.stack-column-center {
+				display: block !important;
+				width: 100% !important;
+				max-width: 100% !important;
+				direction: ltr !important;
+			}
+
+			/* And center justify these ones. */
+			.stack-column-center {
+				text-align: center !important;
+			}
+
+			/* What it does: Generic utility class for centering. Useful for images, buttons, and nested tables. */
+			.center-on-narrow {
+				text-align: center !important;
+				display: block !important;
+				margin-left: auto !important;
+				margin-right: auto !important;
+				float: none !important;
+			}
+
+			table.center-on-narrow {
+				display: inline-block !important;
+			}
+
+			/* What it does: Adjust typography on small screens to improve readability */
+			.email-container p {
+				font-size: 17px !important;
+			}
+		}
+	</style>
+	<!-- Progressive Enhancements : END -->
+
+</head>
+<!--
+	The email background color (#222222) is defined in three places:
+	1. body tag: for most email clients
+	2. center tag: for Gmail and Inbox mobile apps and web versions of Gmail, GSuite, Inbox, Yahoo, AOL, Libero, Comcast, freenet, Mail.ru, Orange.fr
+	3. mso conditional: For Windows 10 Mail
+-->
+
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #222222;">
+	<center style="width: 100%; background-color: #ffffff;">
+		<!--[if mso | IE]>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color: #222222;">
+    <tr>
+    <td>
+    <![endif]-->
+
+		<!-- Visually Hidden Preheader Text : BEGIN -->
+		<div
+			style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+			Request Alert! Request Alert! New request from a potential customer!
+		</div>
+		<!-- Visually Hidden Preheader Text : END -->
+
+		<!-- Create white space after the desired preview text so email clients don’t pull other distracting text into the inbox preview. Extend as necessary. -->
+		<!-- Preview Text Spacing Hack : BEGIN -->
+		<div
+			style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+			&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+		</div>
+		<!-- Preview Text Spacing Hack : END -->
+
+		<!-- Email Body : BEGIN -->
+		<table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="600"
+			style="margin: auto;" class="email-container">
+			<!-- Email Header : BEGIN -->
+			<tr>
+				<td style="padding: 20px 0; text-align: center">
+					<img src="https://arnergy.com/logo.png" width="200" height="50" alt="alt_text" border="0"
+						style="height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">
+				</td>
+			</tr>
+			<!-- Email Header : END -->
+
+			<!-- Hero Image, Flush : BEGIN -->
+			<tr>
+				<td style="background-color: #ffffff;">
+					<img src="https://images.wallpaperscraft.com/image/abstraction_sharp_figure_101902_1920x1080.jpg" width="600"
+						height="" alt="email_img" border="0"
+						style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555; margin: auto; display: block;"
+						class="g-img">
+					<!-- http://www.platinumwebsolutions.co.za/wp-content/uploads/2018/05/Contact-Us.jpg -->
+				</td>
+			</tr>
+			<!-- Hero Image, Flush : END -->
+
+			<!-- 1 Column Text + Button : BEGIN -->
+			<tr>
+				<td style="background-color: #ffffff;">
+					<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+						<tr>
+							<td
+								style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+								<h1
+									style="margin: 0 0 10px; font-size: 25px; line-height: 30px; color: #333333; font-weight: normal; text-align: center;">
+									Hey! you have a new contact! ${values.fullname}!</h1>
+								<table style="  justify-items: center !important;
+                                    width: 100% !important;
+                                    padding: 30px 30px !important;
+                                    border: none;">
+
+									<tr style="line-height: 3rem>
+										<td>Name</td>
+										<td> ${values.fullname} </td>
+									</tr>
+									<tr style="line-height: 3rem>
+										<td>Phone</td>
+										<td>${values.email}</td>
+									</tr>
+                                    <tr style="line-height: 3rem;">
+										<td>Email</td>
+										<td>${values.message}</td>
+									</tr>
+                                   
+									
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding: 0 20px 20px;">
+								<!-- Button : BEGIN -->
+								<table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0"
+									style="margin: auto;">
+									<tr>
+										<td class="button-td button-td-primary"
+											style="border-radius: 4px; background:  #ff7a0f; ">
+											<a class="button-a button-a-primary" href="mailto:${values.email}"
+												style="background:  ##1e87f0; border: 1px solid  #1e87f0; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px; ">Reply</a>
+										</td>
+									</tr>
+								</table>
+								<!-- Button : END -->
+							</td>
+						</tr>
+
+					</table>
+				</td>
+			</tr>
+	</center>
+</body>
+</html>
+        `,
+        altText: `Quote Request 
+                  NAME: ${values.fullname}
+                  EMAIL: ${values.email}
+                  MESSAGE: ${values.message}`,
+        to: `requestquote@arnergy.com`
+
+
+
+    };
+
+
+    // post /CustomEmails/sendEmail
+    $.post(
+        param,
+        function (resp) {
+            // console.log(resp);
+            $('input').val('');
+            $("form").on('submit', submitHandler)
+            $('.btn-submit').val('Request Submitted!')
+            const element = document.querySelector('.btn-submit')
+            element.classList.add('animated', 'zoomOutUp')
+            // $('btn-submit').add('animated', 'slideOutRight')
+            // setTimeout(function(){window.location.reload();}, 500);
+
+
+
+        }
+    ); 
+
+
+
+}
+$("form").on('submit', submitHandler)
--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -5,62 +5,62 @@
 
         var animate = setInterval(function () {
 
-            bar.value = 90;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-css3');
         var animate = setInterval(function () {
 
-            bar.value = 80;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-js');
         var animate = setInterval(function () {
 
-            bar.value = 70;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-vue');
         var animate = setInterval(function () {
 
-            bar.value = 70;
+            bar.value +=10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-react');
         var animate = setInterval(function () {
 
-            bar.value = 50;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-py');
         var animate = setInterval(function () {
 
-            bar.value = 60;
+            bar.value += 10;
 
             if (bar.value >= bar.max) {
                 clearInterval(animate);
@@ -71,22 +71,22 @@
         var bar = document.getElementById('js-progressbar-node');
         var animate = setInterval(function () {
 
-            bar.value = 50;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
         var bar = document.getElementById('js-progressbar-ui');
         var animate = setInterval(function () {
 
-            bar.value = 70;
+            bar.value += 10;
 
-            if (bar.value >= bar.max) {
-                clearInterval(animate);
-            }
+            // if (bar.value >= bar.max) {
+            //     clearInterval(animate);
+            // }
 
         }, 1000);
 
